94568bed2d260d0ad5644853721789ae04008c43
Alejandro Ochoa <aeochoa@unicesar.edu.co>
2026-09-05 22:41:32 -0500

    feat: add abstract Product class to the domain

---
+++ src/main/java/com/gamezone/domain/Product.java
@@ -0,0 +1,42 @@
+package com.gamezone.domain;
+public abstract class Product{
+    private String identifier;
+    private String title;
+    private double price;
+    private int availableQuantify;
+
+
+    public String getIdentifier(){
+        return identifier;
+    }
+    public void setIdentifier(String identifier){
+        this.identifier=identifier;
+    }
+
+
+    public String getTitle(){
+        return title;
+    }
+
+    public void setTitle(String title){
+        this.title=title;
+    }
+
+
+    public double getPrice(){
+        return price;
+    }
+    public void setPrice(double price){
+        this.price=price;
+    }
+
+
+    public int getavailableQuantify(){
+        return availableQuantify;
+    }
+    public void setAvailableQuantify(int availableQuantify){
+        this.availableQuantify=availableQuantify;
+    }
+
+    public abstract String getDescription();
+}
