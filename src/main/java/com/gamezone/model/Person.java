453236f14665d8303422ccd4b15c31577bb9030c
Eusidcarrillo <161573528+Eusidcarrillo@users.noreply.github.com>
2026-09-06 21:18:52 -0500

    feat: add constructor to Person class

--- src/main/java/com/gamezone/model/Person.java
+++ src/main/java/com/gamezone/model/Person.java
@@ -6,4 +6,9 @@ public abstract class Person {
     private String identification;
     private String phone;
 
+    public Person(String name, String identification, String phone) {
+        this.name = name;
+        this.identification = identification;
+        this.phone = phone;
+    }
 }
