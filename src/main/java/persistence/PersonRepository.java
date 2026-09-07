07f24366e10c611865d2a12c4c217e6f71f12191
Eusidcarrillo <161573528+Eusidcarrillo@users.noreply.github.com>
2026-09-06 22:19:19 -0500

    feat: add people list to PersonRepository

--- src/main/java/persistence/PersonRepository.java
+++ src/main/java/persistence/PersonRepository.java
@@ -1,4 +1,15 @@
 package persistence;
 
+import com.gamezone.model.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PersonRepository {
+
+    private List<Person> people;
+
+    public PersonRepository() {
+        this.people = new ArrayList<>();
+    }
 }
