118aa5864bff1e650155beb02f3a8ffbf33ed8da
Eusidcarrillo <161573528+Eusidcarrillo@users.noreply.github.com>
2026-09-06 21:58:22 -0500

    feat: add constructor to Seller class

--- src/main/java/com/gamezone/model/Seller.java
+++ src/main/java/com/gamezone/model/Seller.java
@@ -4,4 +4,11 @@ public class Seller extends Person{
 
     private String employeeCode;
     private String workShift;
+
+    public Seller(String name, String identification, String phone, String employeeCode, String workShift) {
+        super(name, identification, phone);
+        this.employeeCode = employeeCode;
+        this.workShift = workShift;
+    }
+
 }
