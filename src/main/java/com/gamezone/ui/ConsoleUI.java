f4c9665e1695db39bbdffcf188912113249d0efd
Eusidcarrillo <161573528+Eusidcarrillo@users.noreply.github.com>
2026-09-06 22:55:31 -0500

    feat: add PersonService and Scanner attributes to ConsoleUI

--- src/main/java/com/gamezone/ui/ConsoleUI.java
+++ src/main/java/com/gamezone/ui/ConsoleUI.java
@@ -1,4 +1,12 @@
 package com.gamezone.ui;
 
+import com.gamezone.service.PersonService;
+
+import java.util.Scanner;
+
 public class ConsoleUI {
+
+    private PersonService personService;
+    private Scanner scanner;
+
 }
