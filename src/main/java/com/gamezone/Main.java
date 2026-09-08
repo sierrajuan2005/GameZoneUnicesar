d958948faea3424ff51abb64d702bbeeb6a5fb31
Eusidcarrillo <161573528+Eusidcarrillo@users.noreply.github.com>
2026-09-07 20:23:38 -0500

    feat: instantiate PersonService in Main class

--- src/main/java/com/gamezone/Main.java
+++ src/main/java/com/gamezone/Main.java
@@ -1,7 +1,11 @@
 package com.gamezone;
 
+import com.gamezone.service.PersonService;
+
 public class Main {
     public static void main(String[] args) {
+
+        PersonService service = new PersonService();
 
     }
 }
