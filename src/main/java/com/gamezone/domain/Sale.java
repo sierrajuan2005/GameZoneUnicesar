03d989ff84b19db3fdfc62e9dbeeee1012b4149d
Juan Diego Sierra Teran <jdiegosierra@unicesar.edu.co>
2026-09-07 22:34:03 -0500

    feat: add Sale attributes

--- src/main/java/com/gamezone/domain/Sale.java
+++ src/main/java/com/gamezone/domain/Sale.java
@@ -1,4 +1,14 @@
 package com.gamezone.domain;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public class Sale {
+
+    private LocalDate date;
+    private Customer customer;
+    private Seller seller;
+    private List<product> products;
+
 }
+
