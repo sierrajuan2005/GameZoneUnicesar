d56c506d8742dc00c3936b4e2878cdc98e91c552
Eusidcarrillo <161573528+Eusidcarrillo@users.noreply.github.com>
2026-09-07 20:40:47 -0500

    feat: integrate ConsoleUI with PersonService in Main

--- src/main/java/com/gamezone/Main.java
+++ src/main/java/com/gamezone/Main.java
@@ -1,11 +1,14 @@
 package com.gamezone;
 
 import com.gamezone.service.PersonService;
+import com.gamezone.ui.ConsoleUI;
 
 public class Main {
     public static void main(String[] args) {
 
         PersonService service = new PersonService();
+
+        ConsoleUI ui = new ConsoleUI(service);
 
     }
 }
