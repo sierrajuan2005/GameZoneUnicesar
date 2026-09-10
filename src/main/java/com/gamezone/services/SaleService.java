219048cdb54aac4a27b864867e1e44294537aa5e
Juan Diego Sierra Teran <jdiegosierra@unicesar.edu.co>
2026-09-10 11:52:29 -0500

    feat: validate products in sale

--- src/main/java/com/gamezone/services/SaleService.java
+++ src/main/java/com/gamezone/services/SaleService.java
@@ -1,7 +1,16 @@
 package com.gamezone.services;
-
+import com.gamezone.model.Customer;
+import com.gamezone.model.Product;
+import com.gamezone.model.Sale;
+import com.gamezone.model.Seller;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import com.gamezone.persistence.ProductRepository;
 import com.gamezone.persistence.SaleRepository;
+import com.gamezone.services.ProductService;
 
 public class SaleService {
     private SaleRepository saleRepository;
@@ -15,5 +24,16 @@ public class SaleService {
         this.personService = personService;
     }
 
+    public void registerSale(Sale sale) {
+        List<Product> soldProducts = sale.getProducts();
+
+        if (soldProducts.isEmpty()) {
+            throw new IllegalArgumentException("A sale must contain at least one product.");
+        }
+
+        List<Product> storedProducts = productService.listProducts();
+
+
+    }
 
 }
