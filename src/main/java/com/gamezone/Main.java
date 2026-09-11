49b581abe8783ee794ef543d21ebfa8b399eb773
Juan Diego Sierra Teran <jdiegosierra@unicesar.edu.co>
2026-09-10 23:00:36 -0500

    feat: wire repositories and services

--- src/main/java/com/gamezone/Main.java
+++ src/main/java/com/gamezone/Main.java
@@ -1,16 +1,26 @@
 package com.gamezone;
-
+import com.gamezone.model.Person;
+import com.gamezone.model.Seller;
+import com.gamezone.persistence.PersonRepository;
+import com.gamezone.persistence.ProductRepository;
+import com.gamezone.persistence.SaleRepository;
 import com.gamezone.services.PersonService;
+import com.gamezone.services.ProductService;
+import com.gamezone.services.SaleService;
 import com.gamezone.ui.ConsoleUI;
+
 
 public class Main {
     public static void main(String[] args) {
 
-        PersonService service = new PersonService();
+        PersonRepository personRepository = new PersonRepository();
+        ProductRepository productRepository = new ProductRepository();
+        SaleRepository saleRepository = new SaleRepository();
 
-        ConsoleUI ui = new ConsoleUI(service);
+        PersonService personService = new PersonService(personRepository);
+        ProductService productService = new ProductService(productRepository);
+        SaleService saleService = new SaleService(saleRepository, productService);
 
-        ui.start();
 
     }
 }
