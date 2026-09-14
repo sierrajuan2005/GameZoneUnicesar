52076d05a12d55e2962f7e470e715ba982c0614f
Eusidcarrillo <161573528+Eusidcarrillo@users.noreply.github.com>
2026-09-14 15:08:45 -0500

    feat: add internal list and constructor with SaleRepository and ProductRepository

--- src/main/java/com/gamezone/persistence/WarrantyRepository.java
+++ src/main/java/com/gamezone/persistence/WarrantyRepository.java
@@ -1,7 +1,22 @@
 package com.gamezone.persistence;
 
+import com.gamezone.model.Warranty;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class WarrantyRepository {
 
     private static final String FILE_PATH = "data/warranties.csv";
+
+    private List<Warranty> warranties;
+    private SaleRepository saleRepository;
+    private ProductRepository productRepository;
+
+    public WarrantyRepository(SaleRepository saleRepository, ProductRepository productRepository) {
+        this.saleRepository = saleRepository;
+        this.productRepository = productRepository;
+        this.warranties = new ArrayList<>();
+    }
 
 }
