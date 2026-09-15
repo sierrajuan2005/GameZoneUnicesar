26896a37a4cb83a3fa1f668f5dfe7c392f1a8913
Eusidcarrillo <161573528+Eusidcarrillo@users.noreply.github.com>
2026-09-15 13:13:12 -0500

    feat: add convertToCsv method to WarrantyRepository

--- src/main/java/com/gamezone/persistence/WarrantyRepository.java
+++ src/main/java/com/gamezone/persistence/WarrantyRepository.java
@@ -1,7 +1,14 @@
 package com.gamezone.persistence;
 
+import com.gamezone.model.BasicWarranty;
+import com.gamezone.model.ExtendedWarranty;
+import com.gamezone.model.Product;
 import com.gamezone.model.Warranty;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,6 +33,22 @@ public class WarrantyRepository {
 
     public List<Warranty> getAllWarranties(){
         return warranties;
+    }
+
+    private String convertToCsv (Warranty w){
+
+        if (w instanceof BasicWarranty b){
+         return "BASIC;" + b.getWarrantyIdentifier() + ";" +
+         b.getAssociatedProduct().getIdentifier() + ";" +
+         b.getAssociatedSale().getIdentifier() + ";" +
+         b.getStartDate() + ";" + b.getEndDate();
+        } else if (w instanceof ExtendedWarranty e) {
+            return "EXTENDED;" +e.getWarrantyIdentifier() + ";" +
+                    e.getAssociatedProduct().getIdentifier() + ";" +
+                    e.getAssociatedSale().getIdentifier() + ";" +
+                    e.getStartDate() + ";" + e.getEndDate();
+        }
+        return "";
     }
 
 
