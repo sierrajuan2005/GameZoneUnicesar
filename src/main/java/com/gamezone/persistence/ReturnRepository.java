2518afd15c335014a042c7fdb32d5d40a059b107
Eusidcarrillo <161573528+Eusidcarrillo@users.noreply.github.com>
2026-09-17 08:00:29 -0500

    feat: add dependencies and constructor to ReturnRepository

--- src/main/java/com/gamezone/persistence/ReturnRepository.java
+++ src/main/java/com/gamezone/persistence/ReturnRepository.java
@@ -1,4 +1,16 @@
 package com.gamezone.persistence;
 
+import com.gamezone.services.ProductService;
+import com.gamezone.services.SaleService;
+
 public class ReturnRepository {
+
+    private static final String FILE_PATH = "data/returns.csv";
+    private final SaleService saleService;
+    private final ProductService productService;
+
+    public ReturnRepository(SaleService saleService, ProductService productService) {
+        this.saleService = saleService;
+        this.productService = productService;
+    }
 }
