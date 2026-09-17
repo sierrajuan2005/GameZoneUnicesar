fa2db097c128e97ee8bc0fe80339923fd02150a4
Eusidcarrillo <161573528+Eusidcarrillo@users.noreply.github.com>
2026-09-17 15:55:11 -0500

    feat: add PromotionRepository field and constructor in PromotionService

--- src/main/java/com/gamezone/services/PromotionService.java
+++ src/main/java/com/gamezone/services/PromotionService.java
@@ -1,4 +1,12 @@
 package com.gamezone.services;
 
+import com.gamezone.persistence.PromotionRepository;
+
 public class PromotionService {
+
+    private final PromotionRepository promotionRepository;
+
+    public PromotionService(PromotionRepository promotionRepository) {
+        this.promotionRepository = promotionRepository;
+    }
 }
