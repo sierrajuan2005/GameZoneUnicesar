01dda44986fc666524527953ac00180adb7c521d
Eusidcarrillo <161573528+Eusidcarrillo@users.noreply.github.com>
2026-09-17 15:45:27 -0500

    feat: add convertToCsv method in PromotionRepository

--- src/main/java/com/gamezone/persistence/PromotionRepository.java
+++ src/main/java/com/gamezone/persistence/PromotionRepository.java
@@ -1,7 +1,36 @@
 package com.gamezone.persistence;
 
+import com.gamezone.model.BulkPurchaseDiscount;
+import com.gamezone.model.CategoryDiscount;
+import com.gamezone.model.PercentageDiscount;
+import com.gamezone.model.Promotion;
+
 public class PromotionRepository {
 
     private static final String FILE_PATH = "data/promotions.csv";
 
+    private String convertToCsv(Promotion p) {
+        if (p instanceof PercentageDiscount pd) {
+            return "PERCENTAGE;" + pd.getIdentifier() + ";" +
+                    pd.getName() + ";" +
+                    pd.getStartDate() + ";" +
+                    pd.getEndDate() + ";" +
+                    pd.getDiscountPercentage();
+        } else if (p instanceof CategoryDiscount cd) {
+            return "CATEGORY;" + cd.getIdentifier() + ";" +
+                    cd.getName() + ";" +
+                    cd.getStartDate() + ";" +
+                    cd.getEndDate() + ";" +
+                    cd.getDiscountPercentage() + ";" +
+                    cd.getTargetCategory();
+        } else if (p instanceof BulkPurchaseDiscount bd) {
+            return "BULK;" + bd.getIdentifier() + ";" +
+                    bd.getName() + ";" +
+                    bd.getStartDate() + ";" +
+                    bd.getEndDate() + ";" +
+                    bd.getMinQuantity() + ";" +
+                    bd.getDiscountPercentage();
+        }
+        return "";
+    }
 }
