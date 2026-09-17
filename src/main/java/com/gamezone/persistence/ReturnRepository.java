6c7635d057786db9621191ed79808d31d1a37deb
Eusidcarrillo <161573528+Eusidcarrillo@users.noreply.github.com>
2026-09-17 08:05:32 -0500

    feat: implement saveAll method in ReturnRepository

--- src/main/java/com/gamezone/persistence/ReturnRepository.java
+++ src/main/java/com/gamezone/persistence/ReturnRepository.java
@@ -1,7 +1,16 @@
 package com.gamezone.persistence;
 
+import com.gamezone.model.Return;
+import com.gamezone.model.Warranty;
 import com.gamezone.services.ProductService;
 import com.gamezone.services.SaleService;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ReturnRepository {
 
@@ -12,5 +21,20 @@ public class ReturnRepository {
     public ReturnRepository(SaleService saleService, ProductService productService) {
         this.saleService = saleService;
         this.productService = productService;
+    }
+
+    private  void saveAll(List<Return> returns){
+        Path path = Paths.get(FILE_PATH);
+        List<String> lines = new ArrayList<>();
+
+        for (Return r : returns){
+            lines.add(convertToCsv(r));
+        }
+        try {
+            Files.createDirectories(path.getParent());
+            Files.write(path, lines);
+        }catch (IOException e){
+            throw new RuntimeException("Error saving returns.", e);
+        }
     }
 }
