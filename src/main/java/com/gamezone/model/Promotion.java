3c2dfc16f83e94c2f9193fa78ec0957d3b6bdf98
Alejandro Ochoa <aeochoa@unicesar.edu.co>
2026-09-17 10:23:13 -0500

    feat: add promotion constructor and accesors

--- src/main/java/com/gamezone/model/Promotion.java
+++ src/main/java/com/gamezone/model/Promotion.java
@@ -11,4 +11,81 @@ public abstract class Promotion {
     private LocalDate startDate;
     private LocalDate endDate;
     
+    /*
+    Creates a new promotion.
+    @param identifier unique promotion identifier
+    @param name promotion name
+    @param startDate promotion start date
+    @param endDate promotion end date
+     */
+    public Promotion(String identifier, String name, LocalDate startDate, LocalDate endDate) {
+        this.identifier = identifier;
+        this.name = name;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+    
+    /*
+    Returns the promotion identifier.
+    @return promotion identifier
+     */
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    /*
+    Updates the promotion identifier.
+    @param identifier new promotion identifier
+     */
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
+    /*
+    Returns the promotion name.
+    @return promotion name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /*
+    Updates the promotion name.
+    @param name new promotion name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /*
+    Returns the promotion start date.
+    @return start date
+     */
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    /*
+    Updates the promotion start date.
+    @param startDate new start date
+     */
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    /*
+    Returns the promotion end date.
+    @return end date
+     */
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    /*
+    Updates the promotion end date.
+    @param endDate new end date
+     */
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
 }
