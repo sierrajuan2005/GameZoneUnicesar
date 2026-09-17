9111c5f477275c0bb74aaccc45aaebb85b507ab6
Eusidcarrillo <161573528+Eusidcarrillo@users.noreply.github.com>
2026-09-17 08:59:18 -0500

    feat: add constructor to ReturnService whith dependencies

--- src/main/java/com/gamezone/services/ReturnService.java
+++ src/main/java/com/gamezone/services/ReturnService.java
@@ -1,4 +1,17 @@
 package com.gamezone.services;
 
+import com.gamezone.persistence.ReturnRepository;
+
 public class ReturnService {
+
+    private final ReturnRepository returnRepository;
+    private final SaleService saleService;
+    private final ProductService productService;
+
+    public ReturnService(ReturnRepository returnRepository, SaleService saleService, ProductService productService) {
+        this.returnRepository = returnRepository;
+        this.saleService = saleService;
+        this.productService = productService;
+    }
+
 }
