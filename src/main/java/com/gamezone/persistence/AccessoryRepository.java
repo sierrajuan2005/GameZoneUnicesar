881ba1c7f4cc2a0c97199dcc4968301cbeddefac
Eusidcarrillo <161573528+Eusidcarrillo@users.noreply.github.com>
2026-09-18 07:09:14 -0500

    feat: implement saveAll method in AccessoryRepository

--- src/main/java/com/gamezone/persistence/AccessoryRepository.java
+++ src/main/java/com/gamezone/persistence/AccessoryRepository.java
@@ -1,5 +1,12 @@
 package com.gamezone.persistence;
 
+import com.gamezone.model.Accessory;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
 public class AccessoryRepository {
 
     private static final String FILE_PATH = "data/accessories.csv";
@@ -7,4 +14,16 @@ public class AccessoryRepository {
     public AccessoryRepository() {
     }
 
+    public void saveAll(List<Accessory> accessories) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(FILE_PATH))) {
+            for (Accessory accessory : accessories) {
+                String type = accessory.getClass().getSimpleName();
+                writer.println(type + ";" + accessory.getIdentifier() + ";" +
+                        accessory.getTitle() + ";" + accessory.getPrice() + ";" +
+                        accessory.getAvailableQuantity());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
