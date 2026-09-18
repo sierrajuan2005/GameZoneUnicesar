c0a7bb80d187284cc29f5124ea743456fa6a8b7a
Alejandro Ochoa <aeochoa@unicesar.edu.co>
2026-09-17 22:21:18 -0500

    feat: add accessory compatibilty accessors

--- src/main/java/com/gamezone/model/Accessory.java
+++ src/main/java/com/gamezone/model/Accessory.java
@@ -9,8 +9,39 @@ public abstract class Accessory  extends Product {
     private List<Console> compatibleConsoles;
     
 
+    /*
+     * Creates a new accessory.
+     *
+     * @param identifier unique accessory identifier
+     * @param title accessory title
+     * @param price accessory price
+     * @param availableQuantity available quantity in inventory
+     * @param compatibleConsoles consoles compatible with the accessory
+     */
     public Accessory(String identifier, String title, double price, int availability, List<Console> compatibleConsoles) {
         super(identifier, title, price, availability);
         this.compatibleConsoles = compatibleConsoles;
     }
+
+    /*
+    * Returns the consoles compatible with the accessory.
+    *
+    * @return the list of compatible consoles
+    */
+    public List<Console> getCompatibleConsoles() {
+        return compatibleConsoles;
+    }
+
+    /*
+    *Updates the consoles compatible with the accessory.
+
+    @param compatibleConsoles the new list of compatible consoles
+    */
+
+    public void setCompatibleConsoles(List<Console> compatibleConsoles) {
+        this.compatibleConsoles = compatibleConsoles;
+    }
+
+    
+
 }
